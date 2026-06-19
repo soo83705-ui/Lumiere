34c40fb5ec8e24854f11d639b893b4d0c384b97a
bada0310 <pppp000310@gmail.com>
2026-06-19 13:42:56 +0900

    ✨[fix]: Correct URL pattern for accounts by adding 'accounts/' prefix

--- back/Lumiere/urls.py
+++ back/Lumiere/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('api/products/', include('products.urls')),
     # path('', include('personal_color.urls')),
     # path('', include('product.urls')),
